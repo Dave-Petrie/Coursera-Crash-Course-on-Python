e36bc6a23843195622f4b522071e82f68f01d6da
David Petrie <petriedk@gmail.com>
2021-02-16 11:41:22 +1000

    Update Functions.py
    Quick testing for user input management

--- Functions.py
+++ Functions.py
@@ -1,3 +1,6 @@
+name = input("Please enter a name of a person \n")
+department = input("Please enter a name of a department \n")
+
 def greeting(name, department):
-    print("WElcome, " + name)
+    print("Welcome, " + name)
     print("You are part of " + department)
